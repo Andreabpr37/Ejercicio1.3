397e1831458b3b72050ee8b5db5e078626b6bbba
Andrea <Andreabpr37@gmail.com>
2021-05-21 16:32:41 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 1;
+            Console.WriteLine("Valor inicial de N = " + N);
+            N = N + 77;
+            Console.WriteLine("N + 77 = " + N);
+            N = N - 3;
+            Console.WriteLine("N - 3 = " + N);
+            N = N * 2;
+            Console.WriteLine("N * 2 = " + N);
+        }
+    }
+}
+
